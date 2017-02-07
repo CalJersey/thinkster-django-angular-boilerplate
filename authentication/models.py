a401384250833a3f509430f27e68db8db4900cfa
CalJersey <dan@vitad.com>
2017-02-07 11:06:57 -0500

    auth model and view. Api end point and initial Angular

--- authentication/models.py
+++ authentication/models.py
@@ -4,6 +4,7 @@ from django.db import models
 # Create your models here.
 
 class AccountManager(BaseUserManager):
+
     def create_user(self, email, password=None, **kwargs):
         if not email:
             raise ValueError('Users must have a valid email address.')
